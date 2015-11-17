bc164f29095d82cd37a3e4f3e4f8c8eda72ad9a3
Sky Su <februaryskys@gmail.com>
2015-11-17 23:30:15 +0000

    Adds runner and relative ruby files

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,105 @@
+#### Example
+
+def return_10
+  10
+end
+
+
+#### Integer Play
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def divide(num1, num2)
+  num1 / num2
+end
+
+
+#### String Play
+
+def length_of_string(str)
+  str.length
+end
+
+def join_string(str1, str2)
+  str1 + str2
+end
+
+
+#### Type Conversion
+
+def add_string_as_number(str1, str2)
+  str1.to_i + str2.to_i
+end
+
+
+#### Conditional Logic
+
+def number_to_full_month_name(num)
+  case num   
+    when 1
+      "January"
+    when 2
+      "February"
+    when 3
+      "March"
+    when 4
+      "April"
+    when 5
+      "May"
+    when 6
+      "June"
+    when 7
+      "July"
+    when 8
+      "August"
+    when 9
+      "September"
+    when 10
+      "October"
+    when 11
+      "November"
+    when 12
+      "December"
+  end
+end
+
+def number_to_short_month_name(num)
+  number_to_full_month_name(num)[0..2]
+end
+
+
+#### Further Challenges
+
+def volume_of_cube(side)
+  side**3
+end
+
+def volume_of_sphere(radius)
+  4/3 * Math::PI * radius**3
+end
+
+def nights_until_christmas(date)
+
+  # Set Christmas Day to Current Year
+  year = Date.today.year
+  xmas_day = Date.new(year,12,25)
+
+  # Set Christmas Day to Next Year if date is past Christmas of current year
+  xmas_day = Date.new(year+1,12,25) if xmas_day - date < 0
+
+  (xmas_day - date).to_i
+end
+
+def age_today(date)
+  ((Date.today - date)/365).to_i
+end    
